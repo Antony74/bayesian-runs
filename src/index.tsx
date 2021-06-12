40570df90477422fb6331c2d9d3eebf4717b3a10
Antony74 <akb@akb.me.uk>
2021-06-12 17:57:27 +0100

    Hello React

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <div>Helloz</div>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
