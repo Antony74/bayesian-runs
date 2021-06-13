f1ee806516c77cf7a376fcfa6509ea6cee7f2781
Antony74 <akb@akb.me.uk>
2021-06-13 11:59:27 +0100

    Prettify and get this thing to compile

--- src/bayes.ts
+++ src/bayes.ts
@@ -1,136 +1,108 @@
-
-function wrapUnary(fn)
-{
-	return function(a)
-	{
-		if (typeof(a) === 'number')
-		{
-			return fn(a);
-		}
-		else if (Array.isArray(a))
-		{
-			var arrOut = Array(a.length);
-			for (var n = 0; n < a.length; ++n)
-			{
-				arrOut[n] = fn(a[n]);
-			}
-			return arrOut;
-		}
-		else
-		{
-			throw 'Not supported';
-		}
-	};
+function wrapUnary(fn: (value: number) => number) {
+  return function (a: number | number[]) {
+    if (typeof a === 'number') {
+      return fn(a);
+    } else if (Array.isArray(a)) {
+      var arrOut = Array(a.length);
+      for (var n = 0; n < a.length; ++n) {
+        arrOut[n] = fn(a[n]);
+      }
+      return arrOut;
+    } else {
+      throw 'Not supported';
+    }
+  };
 }
 
-function wrapBinary(fn)
-{
-	return function(a, b)
-	{
-		var arrOut, n;
+function wrapBinary(fn: (value1: number, value2: number) => number) {
+  return function (a: number | number[], b: number | number[]): number[] {
+    var arrOut, n;
 
-		if (typeof(a) === 'number' && typeof(b) === 'number')
-		{
-			return fn(a,b);
-		}
-		else if (typeof(a) === 'number' && Array.isArray(b))
-		{
-			arrOut = Array(b.length);
-			for (n = 0; n < b.length; ++n)
-			{
-				arrOut[n] = fn(a, b[n]);
-			}
-			return arrOut;
-		}
-		else if (Array.isArray(a) && typeof(b) === 'number')
-		{
-			arrOut = Array(a.length);
-			for (n = 0; n < a.length; ++n)
-			{
-				arrOut[n] = fn(a[n], b);
-			}
-			return arrOut;
-		}
-		else if (Array.isArray(a) && Array.isArray(b))
-		{
-			if (a.length !== b.length)
-			{
-				throw 'Array lengths differ';
-			}
+    if (typeof a === 'number' && typeof b === 'number') {
+      return [fn(a, b)];
+    } else if (typeof a === 'number' && Array.isArray(b)) {
+      arrOut = Array(b.length);
+      for (n = 0; n < b.length; ++n) {
+        arrOut[n] = fn(a, b[n]);
+      }
+      return arrOut;
+    } else if (Array.isArray(a) && typeof b === 'number') {
+      arrOut = Array(a.length);
+      for (n = 0; n < a.length; ++n) {
+        arrOut[n] = fn(a[n], b);
+      }
+      return arrOut;
+    } else if (Array.isArray(a) && Array.isArray(b)) {
+      if (a.length !== b.length) {
+        throw 'Array lengths differ';
+      }
 
-			arrOut = Array(a.length);
-			for (n = 0; n < a.length; ++n)
-			{
-				arrOut[n] = fn(a[n], b[n]);
-			}
-			return arrOut;
-		}
-		else
-		{
-			throw 'Not supported';
-		}
-	};
+      arrOut = Array(a.length);
+      for (n = 0; n < a.length; ++n) {
+        arrOut[n] = fn(a[n], b[n]);
+      }
+      return arrOut;
+    } else {
+      throw 'Not supported';
+    }
+  };
 }
 
 // Define various wrappers
-var div = wrapBinary( function(a,b) {return a/b;} );
-var mul = wrapBinary( function(a,b) {return a*b;} );
-var pow = wrapBinary( Math.pow );
+var div = wrapBinary(function (a, b) {
+  return a / b;
+});
+var mul = wrapBinary(function (a, b) {
+  return a * b;
+});
+var pow = wrapBinary(Math.pow);
 
-var neg  = wrapUnary( function(a) {return -a;} );
-var exp  = wrapUnary( Math.exp );
+var neg = wrapUnary(function (a) {
+  return -a;
+});
+var exp = wrapUnary(Math.exp);
 
-function factorial(x)
-{
-	var f = 1;
-	for (var n = 1; n <= x; ++n)
-	{
-		f *= n;
-	}
+function factorial(x: number) {
+  var f = 1;
+  for (var n = 1; n <= x; ++n) {
+    f *= n;
+  }
 
-	return f;
+  return f;
 }
 
-function sum(arr)
-{
-	var result = 0;
+function sum(arr: number[]) {
+  var result = 0;
 
-	for (var n = 0; n < arr.length; ++n)
-	{
-		result += arr[n];
-	}
+  for (var n = 0; n < arr.length; ++n) {
+    result += arr[n];
+  }
 
-	return result;
+  return result;
 }
 
-function possion(x, lambda)
-{
-	// Only do it this way for very small values of x
-	return div(
-				mul( exp( neg(lambda)),
-				     pow(lambda, x)),
-				factorial(x));
+function possion(x: number, lambda: number | number[]) {
+  // Only do it this way for very small values of x
+  return div(mul(exp(neg(lambda)), pow(lambda, x)), factorial(x));
 }
 
-function bayes(likelihoods, priors)
-{
-	var lp = mul(likelihoods, priors);
-	var evidence = sum(lp);
-	return div(lp, evidence);
+function bayes(likelihoods: number | number[], priors: number | number[]) {
+  const lp = mul(likelihoods, priors);
+  const evidence = sum(lp);
+  return div(lp, evidence);
 }
 
 // Choose a sample of possible lambda
-var lambda = [];
-for (var n = 0.1; n <= 20; n += 0.1)
-{
-	lambda.push(n);
+const lambda: number[] = [];
+for (var n = 0.1; n <= 20; n += 0.1) {
+  lambda.push(n);
 }
 
 var N = lambda.length;
 
 // Choose some priors
 var priors = Array(N);
-priors.fill(1/N);
+priors.fill(1 / N);
 
 // Calculate likelihoods
 var likelihoods = possion(2, lambda);
@@ -138,12 +110,9 @@ var likelihoods = possion(2, lambda);
 // Calculate posteriors
 var posteriors = bayes(likelihoods, priors);
 
-function p(t)
-{
-	var possibities = exp(neg(mul(lambda, t)));
-	return sum(mul(possibities, posteriors));
+function p(t: number) {
+  const possibities = exp(neg(mul(lambda, t)));
+  return sum(mul(possibities, posteriors));
 }
 
 console.log('p(1.72)=' + p(1.72));
-
-
