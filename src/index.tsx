59354923e1c43f14f0d0c75d7b88c8656baa8ba9
Antony74 <akb@akb.me.uk>
2021-06-27 18:36:39 +0100

    Fix console warning

--- src/index.tsx
+++ src/index.tsx
@@ -62,26 +62,26 @@ const App = () => {
           <Box>
             <Typography>
               I have attempted to fix this transient bug. Assume that:
-              <Box {...indentProps}>
-                <List>
-                  <ListItemText>
-                    • The program is failing randomly, with a fixed (but
-                    unknown) chance of failure.
-                  </ListItemText>
-                  <ListItemText>
-                    • Before the first time I ran the original program, all
-                    failure probabilities were equally likely.
-                  </ListItemText>
-                  <ListItemText>
-                    • An unsuccessful fix will have no effect on the frequency
-                    with which the problem is occurring.
-                  </ListItemText>
-                  <ListItemText>
-                    • The problem will never re-occur if the fix is successful.
-                  </ListItemText>
-                </List>
-              </Box>
             </Typography>
+            <Box {...indentProps}>
+              <List>
+                <ListItemText>
+                  • The program is failing randomly, with a fixed (but unknown)
+                  chance of failure.
+                </ListItemText>
+                <ListItemText>
+                  • Before the first time I ran the original program, all
+                  failure probabilities were equally likely.
+                </ListItemText>
+                <ListItemText>
+                  • An unsuccessful fix will have no effect on the frequency
+                  with which the problem is occurring.
+                </ListItemText>
+                <ListItemText>
+                  • The problem will never re-occur if the fix is successful.
+                </ListItemText>
+              </List>
+            </Box>
           </Box>
           <Box>
             <BChart hook={hook}></BChart>
